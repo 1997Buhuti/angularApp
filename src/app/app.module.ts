4d178dacac52e92fc4b170b84e2bf66f02418b15
Manakal <dp.manakal82@gmail.com>
2022-02-05 18:52:33 +0530

    Nav Bar added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+
+import { RecipeDetailsComponent } from './components/Recipe/recipe-details/recipe-details.component';
+import { ShoppingListEditComponent } from './components/Shopping/shoppingList/shopping-list-edit/shopping-list-edit.component';
+import {ShoppingListComponent} from './components/Shopping/shopping-list/shopping-list.component';
+import {RecipeComponent}  from './components/Recipe/Recipe.component';
+import {RecipeItemComponent}  from './components/Recipe/recipeList/recipe-item/recipe-item.component';
+import {RecipeListComponent}  from './components/Recipe/recipeList/recipe-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    RecipeDetailsComponent,
+    ShoppingListEditComponent,
+    ShoppingListComponent,
+    RecipeComponent,
+    RecipeItemComponent,
+    RecipeListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/Recipe/Recipe.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-recipe',
+  templateUrl: './Recipe.component.html',
+  styleUrls: ['./Recipe.component.scss']
+})
+export class RecipeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
